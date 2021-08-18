97be41e73315eb684a27591025546b4f2a0d325b
Andrey Shustov <andrejshusto@mail.ru>
2021-08-18 16:19:45 +0300

    Rounded view

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLightView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.layer.cornerRadius = 50
+        
     }
 
 
